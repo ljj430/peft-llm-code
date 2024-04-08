8c9ebf1d1440381d6abe563f8a4f651517efc0ae
ljj430 <ljj790225650@gmail.com>
2024-04-07 23:28:15 -0400

    test

--- scripts/run_test_codellam_remote.sh
+++ scripts/run_test_codellam_remote.sh
@@ -8,9 +8,9 @@
 # module load cuda/12.2.2
 # source activate latest_peft
 # nvidia-smi
-python3 -m wandb login 722f6e2de6b117fb0368a2a882b866fed379eb48
-WANDB__SERVICE_WAIT=300
-CUDA_VISIBLE_DEVICES=0 python ../main.py \
+# python3 -m wandb login 722f6e2de6b117fb0368a2a882b866fed379eb48
+# WANDB__SERVICE_WAIT=300
+CUDA_VISIBLE_DEVICES=0 python3 ../main.py \
 --model_name_or_path "codellama/CodeLlama-7b-hf" \
 --dataset "codealpaca" \
 --tuning_method "prompt-tuning" \
